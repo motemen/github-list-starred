1e983b86b2aa0f87984fcdc5e0e45e9d83232462
motemen <motemen@gmail.com>
2014-08-23 11:51:01 +0900

    init

---
+++ main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"code.google.com/p/goauth2/oauth"
+
+	"github.com/google/go-github/github"
+)
+
+func main() {
+	options := &github.ActivityListStarredOptions{Sort: "created"}
+
+	if len(os.Args) <= 1 {
+		log.Fatalf("Usage: %s <GitHub username>", os.Args[0])
+	}
+	user := os.Args[1]
+
+	client := newClient()
+
+	for page := 1; ; page++ {
+		options.Page = page
+
+		repositories, res, err := client.Activity.ListStarred(user, options)
+		if err != nil {
+			log.Fatalf("ListStarred: %s", err)
+		}
+
+		log.Printf("page: %d/%d", page, res.LastPage)
+		for _, repo := range repositories {
+			fmt.Println(*repo.HTMLURL)
+		}
+
+		if page >= res.LastPage {
+			break
+		}
+	}
+}
+
+func newClient() *github.Client {
+	githubToken := os.Getenv("GITHUB_TOKEN")
+
+	if githubToken != "" {
+		oauthTransport := &oauth.Transport{
+			Token: &oauth.Token{AccessToken: githubToken},
+		}
+		return github.NewClient(oauthTransport.Client())
+	}
+
+	return github.NewClient(nil)
+}
